889733237c748991af75fbcc4e03b65c68bda436
Tobias <tobias.lino@fatec.sp.gov.br>
2020-10-16 22:41:55 -0300

    adição dos métodos findById() e findAll

--- ddc-controller/src/main/java/com/tecsus/ddc/controller/service/BillService.java
+++ ddc-controller/src/main/java/com/tecsus/ddc/controller/service/BillService.java
@@ -1,13 +1,10 @@
 package com.tecsus.ddc.controller.service;
 
-import com.tecsus.ddc.bills.Bill;
-
 import java.util.List;
+import java.util.Optional;
 
 public interface BillService {
 
-    List<Bill> select(final String whereStatement, final String whereValue);
-    void insert(Bill bill);
-    void update(long billId, Bill bill);
-    void delete(long billId);
+    Optional<Object> findById(String idBill);
+    <T> List<T> findAll();
 }
