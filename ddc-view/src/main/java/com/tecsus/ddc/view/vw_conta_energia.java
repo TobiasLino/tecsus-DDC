434534584ea7943dd77c46cf2808c445d1ba81f7
RCLMacedo07 <ruan.macedo@fatec.sp.gov.br>
2020-10-17 22:32:30 -0300

    Criando formulario de cadastro de conta de energia

---
+++ ddc-view/src/main/java/com/tecsus/ddc/view/vw_conta_energia.java
@@ -0,0 +1,38 @@
+package com.tecsus.ddc.view;
+
+import javax.swing.*;
+
+public class vw_conta_energia extends JFrame{
+    private JPanel panel1;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JTextField textField5;
+    private JTextField textField6;
+    private JTextField textField7;
+    private JTextField textField8;
+    private JTextField textField9;
+    private JTextField textField10;
+    private JTextField textField11;
+    private JTextField textField12;
+    private JTextField textField13;
+    private JTextField textField14;
+    private JTextField textField15;
+    private JTextField textField16;
+    private JTextField textField17;
+    private JTextField textField18;
+    private JTextField textField19;
+    private JTextField textField20;
+    private JTextField textField21;
+    private JTextField textField22;
+    private JTextField textField23;
+    private JTextField textField24;
+    private JTextField textField25;
+    private JTextField textField26;
+    private JTextField textField27;
+    private JTextField textField28;
+    private JTextField textField29;
+    private JTextField textField30;
+    private JButton salvarButton;
+}
