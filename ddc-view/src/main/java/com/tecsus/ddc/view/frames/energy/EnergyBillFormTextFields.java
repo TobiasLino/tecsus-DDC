70361d5aa91354450aab75ad8cca19f2219faf17
WilliamAntoniazzi <william.antonaizzi@fatec.sp.gov.br>
2020-11-14 23:24:09 -0300

    Tela de Energia

---
+++ ddc-view/src/main/java/com/tecsus/ddc/view/frames/energy/EnergyBillFormTextFields.java
@@ -0,0 +1,156 @@
+package com.tecsus.ddc.view.frames.energy;
+
+import javax.swing.JTextField;
+import javax.swing.JFormattedTextField;
+//import javax.swing.*;
+
+public class EnergyBillFormTextFields {
+    
+    private JTextField txtBairroEnd;
+    private JTextField txtBaseCalculoCofins;
+    private JTextField txtBaseCalculoICMS;
+    private JTextField txtBaseCalculoPIS;
+    private JTextField txtCIPMunicipal;
+    private JTextField txtCepEnd;
+    private JTextField txtCidadeEnd;
+    private JTextField txtClasseSubclasse;
+    private JTextField txtCodFiscalOperacao;
+    private JTextField txtCodIdentificacao;
+    private JTextField txtCodigoBarras;
+    private JTextField txtComplementoEnd;
+    private JTextField txtConsumoKwhMes;
+    private JTextField txtContaMes;
+    private JTextField txtCpfCnpj;
+    private JTextField txtDataFinalBandeira;
+    private JTextField txtDataInicioBandeira;
+    private JTextField txtDataLeitAnterior;
+    private JTextField txtDataLeitAtual;
+    private JTextField txtDataLeitProxima;
+    private JTextField txtDataVencimento;
+    private JTextField txtDiasBandeira;
+    private JTextField txtDiasFatura;
+    private JTextField txtEmissaoFatura;
+    private JTextField txtEndereco;
+    private JTextField txtGrupoSubgrupo;
+    private JTextField txtInsEstadual;
+    private JTextField txtLeituraAntKwh;
+    private JTextField txtLeituraAtualKwh;
+    private JTextField txtModalidadeTarifa;
+    private JTextField txtNomeClienteEnd;
+    private JTextField txtNumeroEnd;
+    private JTextField txtNumeroInstalacao;
+    private JTextField txtNumeroMedidor;
+    private JTextField txtRazaoSocial;
+    private JTextField txtRoteiroLeitura;
+    private JTextField txtTensaoNominal;
+    private JTextField txtTipoBandeira;
+    private JTextField txtTipoFornecimento;
+    private JTextField txtValorCofins;
+    private JTextField txtValorFornTE;
+    private JTextField txtValorFornTusd;
+    private JTextField txtValorICMS;
+    private JTextField txtValorPIS;
+    private JTextField txtValorTotalPagar;
+    private JTextField txtValorTotalTE;
+    private JTextField txtValorTotalTusd;
+
+    public EnergyBillFormTextFields() {
+        this.txtBairroEnd = new JTextField();
+        this.txtBaseCalculoCofins = new JTextField();
+        this.txtBaseCalculoICMS = new JTextField();
+        this.txtBaseCalculoPIS = new JTextField();
+        this.txtCIPMunicipal = new JTextField();
+        this.txtCepEnd = new JTextField();
+        this.txtCidadeEnd = new JTextField();
+        this.txtClasseSubclasse = new JTextField();
+        this.txtCodFiscalOperacao = new JTextField();
+        this.txtCodIdentificacao = new JTextField();
+        this.txtCodigoBarras = new JTextField();
+        this.txtComplementoEnd = new JTextField();
+        this.txtConsumoKwhMes = new JTextField();
+        this.txtContaMes = new JTextField();
+        this.txtCpfCnpj = new JTextField();
+        this.txtDataFinalBandeira = new JTextField();
+        this.txtDataInicioBandeira = new JTextField();
+        this.txtDataLeitAnterior = new JTextField();
+        this.txtDataLeitAtual = new JTextField();
+        this.txtDataLeitProxima = new JTextField();
+        this.txtDataVencimento = new JTextField();
+        this.txtDiasBandeira = new JTextField();
+        this.txtDiasFatura = new JTextField();
+        this.txtEmissaoFatura = new JTextField();
+        this.txtEndereco = new JTextField();
+        this.txtGrupoSubgrupo = new JTextField();
+        this.txtInsEstadual = new JTextField();
+        this.txtLeituraAntKwh = new JTextField();
+        this.txtLeituraAtualKwh = new JTextField();
+        this.txtModalidadeTarifa = new JTextField();
+        this.txtNomeClienteEnd = new JTextField();
+        this.txtNumeroEnd = new JTextField();
+        this.txtNumeroInstalacao = new JTextField();
+        this.txtNumeroMedidor = new JTextField();
+        this.txtRazaoSocial = new JTextField();
+        this.txtRoteiroLeitura = new JTextField();
+        this.txtTensaoNominal = new JTextField();
+        this.txtTipoBandeira = new JTextField();
+        this.txtTipoFornecimento = new JTextField();
+        this.txtValorCofins = new JTextField();
+        this.txtValorFornTE = new JTextField();
+        this.txtValorFornTusd = new JTextField();
+        this.txtValorICMS = new JTextField();
+        this.txtValorPIS = new JTextField();
+        this.txtValorTotalPagar = new JTextField();
+        this.txtValorTotalTE = new JTextField();
+        this.txtValorTotalTusd = new JTextField();
+    }
+    
+    public JTextField getTxtBairroEnd(){return txtBairroEnd;}
+    public JTextField getTxtBaseCalculoCofins(){return txtBaseCalculoCofins;}
+    public JTextField getTxtBaseCalculoICMS(){return txtBaseCalculoICMS;}
+    public JTextField getTxtBaseCalculoPIS(){return txtBaseCalculoPIS;}
+    public JTextField getTxtCIPMunicipal(){return txtCIPMunicipal;}
+    public JTextField getTxtCepEnd(){return txtCepEnd;}
+    public JTextField getTxtCidadeEnd(){return txtCidadeEnd;}
+    public JTextField getTxtClasseSubclasse(){return txtClasseSubclasse;}
+    public JTextField getTxtCodFiscalOperacao(){return txtCodFiscalOperacao;}
+    public JTextField getTxtCodIdentificacao(){return txtCodIdentificacao;}
+    public JTextField getTxtCodigoBarras(){return txtCodigoBarras;}
+    public JTextField getTxtComplementoEnd(){return txtComplementoEnd;}
+    public JTextField getTxtConsumoKwhMes(){return txtConsumoKwhMes;}
+    public JTextField getTxtContaMes(){return txtContaMes;}
+    public JTextField getTxtCpfCnpj(){return txtCpfCnpj;}
+    public JTextField getTxtDataFinalBandeira(){return txtDataFinalBandeira;}
+    public JTextField getTxtDataInicioBandeira(){return txtDataInicioBandeira;}
+    public JTextField getTxtDataLeitAnterior(){return txtDataLeitAnterior;}
+    public JTextField getTxtDataLeitAtual(){return txtDataLeitAtual;}
+    public JTextField getTxtDataLeitProxima(){return txtDataLeitProxima;}
+    public JTextField getTxtDataVencimento(){return txtDataVencimento;}
+    public JTextField getTxtDiasBandeira(){return txtDiasBandeira;}
+    public JTextField getTxtDiasFatura(){return txtDiasFatura;}
+    public JTextField getTxtEmissaoFatura(){return txtEmissaoFatura;}
+    public JTextField getTxtEndereco(){return txtEndereco;}
+    public JTextField getTxtGrupoSubgrupo(){return txtGrupoSubgrupo;}
+    public JTextField getTxtInsEstadual(){return txtInsEstadual;}
+    public JTextField getTxtLeituraAntKwh(){return txtLeituraAntKwh;}
+    public JTextField getTxtLeituraAtualKwh(){return txtLeituraAtualKwh;}
+    public JTextField getTxtModalidadeTarifa(){return txtModalidadeTarifa;}
+    public JTextField getTxtNomeClienteEnd(){return txtNomeClienteEnd;}
+    public JTextField getTxtNumeroEnd(){return txtNumeroEnd;}
+    public JTextField getTxtNumeroInstalacao(){return txtNumeroInstalacao;}
+    public JTextField getTxtNumeroMedidor(){return txtNumeroMedidor;}
+    public JTextField getTxtRazaoSocial(){return txtRazaoSocial;}
+    public JTextField getTxtRoteiroLeitura(){return txtRoteiroLeitura;}
+    public JTextField getTxtTensaoNominal(){return txtTensaoNominal;}
+    public JTextField getTxtTipoBandeira(){return txtTipoBandeira;}
+    public JTextField getTxtTipoFornecimento(){return txtTipoFornecimento;}
+    public JTextField getTxtValorCofins(){return txtValorCofins;}
+    public JTextField getTxtValorFornTE(){return txtValorFornTE;}
+    public JTextField getTxtValorFornTusd(){return txtValorFornTusd;}
+    public JTextField getTxtValorICMS(){return txtValorICMS;}
+    public JTextField getTxtValorPIS(){return txtValorPIS;}
+    public JTextField getTxtValorTotalPagar(){return txtValorTotalPagar;}
+    public JTextField getTxtValorTotalTE(){return txtValorTotalTE;}
+    public JTextField getTxtValorTotalTusd(){return txtValorTotalTusd;}
+
+
+}
