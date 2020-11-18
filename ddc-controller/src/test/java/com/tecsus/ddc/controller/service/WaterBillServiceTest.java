bd13261680e9102ac2568456771ac567ea8e16cc
TobiasLino <tobias.lino@fatec.sp.gov.br>
2020-11-17 22:33:51 -0300

    teste corrigido

--- ddc-controller/src/test/java/com/tecsus/ddc/controller/service/WaterBillServiceTest.java
+++ ddc-controller/src/test/java/com/tecsus/ddc/controller/service/WaterBillServiceTest.java
@@ -3,8 +3,11 @@ package com.tecsus.ddc.controller.service;
 import com.tecsus.ddc.bills.water.WaterBill;
 import com.tecsus.ddc.controller.connector.ConnectionImpl;
 import com.tecsus.ddc.controller.connector.Connector;
+import com.tecsus.ddc.controller.repository.BillRepository;
 import com.tecsus.ddc.controller.repository.WaterBillRepository;
+import com.tecsus.ddc.factory.BillFactory;
 import com.tecsus.ddc.factory.WaterBillFactory;
+import com.tecsus.ddc.query.AbstractBillQueryFactory;
 import com.tecsus.ddc.query.WaterBillQueryFactory;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
@@ -18,16 +21,18 @@ import java.util.Optional;
 public class WaterBillServiceTest {
 
     private final WaterBillRepository waterBillRepository;
+    private final BillRepository billRepository;
 
     public WaterBillServiceTest() {
         Connector connector = new Connector().connect();
         ConnectionImpl connection = connector.getConnection();
+        this.billRepository = new BillRepository(connection, new AbstractBillQueryFactory(), new BillFactory());
         this.waterBillRepository = new WaterBillRepository(connection, new WaterBillQueryFactory(), new WaterBillFactory());
     }
 
     @Test
     public void findAllTest() {
-        WaterBillService service = new WaterBillService(waterBillRepository);
+        WaterBillService service = new WaterBillService(waterBillRepository, billRepository);
 
         List<WaterBill> list = service.findAll();
         list.forEach(System.out::println);
@@ -35,7 +40,7 @@ public class WaterBillServiceTest {
 
     @Test
     public void findByIdTest() {
-        WaterBillService service = new WaterBillService(waterBillRepository);
+        WaterBillService service = new WaterBillService(waterBillRepository, billRepository);
 
         Optional<WaterBill> billOptional = service.findById("12345678910");
         if (billOptional.isPresent()) {
