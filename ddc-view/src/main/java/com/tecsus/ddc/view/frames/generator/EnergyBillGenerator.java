ea49b03d3e36f9c512186430eb6cdfe433af5c86
TobiasLino <tobias.lino@fatec.sp.gov.br>
2020-11-20 20:12:10 -0300

    inserção de bandeira

--- ddc-view/src/main/java/com/tecsus/ddc/view/frames/generator/EnergyBillGenerator.java
+++ ddc-view/src/main/java/com/tecsus/ddc/view/frames/generator/EnergyBillGenerator.java
@@ -4,22 +4,26 @@ import com.tecsus.ddc.bills.Bill;
 import com.tecsus.ddc.bills.energy.Classe;
 import com.tecsus.ddc.bills.energy.EnergyBill;
 import com.tecsus.ddc.bills.energy.Group;
+import com.tecsus.ddc.bills.energy.TariffFlag;
 import com.tecsus.ddc.bills.energy.enums.*;
 import com.tecsus.ddc.instalation.Instalation;
 import com.tecsus.ddc.view.frames.energy.EnergyBillFormTextFields;
 import lombok.extern.slf4j.Slf4j;
 import org.joda.time.DateTime;
 
+import javax.swing.*;
 import java.math.BigDecimal;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 public class EnergyBillGenerator {
     
     private EnergyBillGenerator() {}
 
-    public static EnergyBill generate(EnergyBillFormTextFields fields) {
+    public static EnergyBill generate(EnergyBillFormTextFields fields, final JTable tariffFlagsTable) {
         //throw new UnsupportedOperationException("Not supported yet.");
         SimpleDateFormat format = new SimpleDateFormat("dd/MM/YYYY");
         SimpleDateFormat refMonthFormatter = new SimpleDateFormat("MM/YYYY");
@@ -61,6 +65,9 @@ public class EnergyBillGenerator {
                     .supplyType(SupplyType.valueOf(fields.getTxtTipoFornecimento().getText()))
                     .tributes(calculaTributos(fields))
                     .build();
+        for (TariffFlag tariffFlag : constructTariffFlags(tariffFlagsTable))
+            res.addTariffFlag(tariffFlag);
+
         log.info("Energy bill generated");
         } catch (ParseException e) {
             e.printStackTrace();
@@ -74,5 +81,30 @@ public class EnergyBillGenerator {
         BigDecimal cofins = new BigDecimal(fields.getTxtValorCofins().getText());
         return pis.add(cofins);
     }
-}
 
+    static private List<TariffFlag> constructTariffFlags(final JTable flagsTable) throws ParseException {
+        List<TariffFlag> tariffFlags = new ArrayList<>();
+
+        SimpleDateFormat format = new SimpleDateFormat("dd/MM/YYYY");
+        String bill = "", flagColor = "", start = "", finish = "";
+
+        for (int i = 0; i < flagsTable.getRowCount(); ++i) {
+            for (int j = 0; j < flagsTable.getColumnCount(); ++j) {
+                bill = (String) flagsTable.getModel().getValueAt(i, j);
+                flagColor = (String) flagsTable.getModel().getValueAt(i, j);
+                start = (String) flagsTable.getModel().getValueAt(i, j);
+                finish = (String) flagsTable.getModel().getValueAt(i, j);
+            }
+            TariffFlag flag = TariffFlag.builder()
+                    .idBill(bill)
+                    .flag(TariffFlags.valueOf(flagColor))
+                    .start(new DateTime(format.parse(start)))
+                    .finish(new DateTime(format.parse(finish)))
+                    .build();
+            bill = ""; flagColor = ""; start = ""; finish = "";
+            tariffFlags.add(flag);
+        }
+
+        return tariffFlags;
+    }
+}
